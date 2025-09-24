3996dcc6d4d2e4953c261f763f35eddeba524044
angelchelssa <angelchelsa150526@gmail.com>
2025-09-24 16:42:36 +0700

    jobsheet 4

--- variabel_konstanta.php/tipe_data.php
+++ variabel_konstanta.php/tipe_data.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 $a = 10;
 $b = 5;
@@ -43,4 +44,51 @@ echo "Nama Depan: {$namaDepan} <br>";
 echo 'Nama Belakang: ' . $namaBelakang . '<br>';
 
 echo $namaLengkap;
+=======
+<?php
+$a = 10;
+$b = 5;
+$c = $a + 5;
+$d = $b + (10 * 5);
+$e = $d - $c;
+
+echo "Variable a : {$a} <br>";
+echo "Variable b : {$b} <br>";
+echo "Variable c : {$c} <br>";
+echo "Variable d : {$d} <br>";
+echo "Variable e : {$e} <br>";
+
+var_dump($e);
+
+$nilaiMatematika = 5.1;
+$nilaiIPA =6.7;
+$nilaiBahasaIndonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) / 3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Rata - rata : {$rataRata} <br>";
+
+var_dump($rataRata);
+
+$apkaahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+var_dump($apkaahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahUjian);
+
+$namaDepan = "Ibnu";
+$namaBelakang = 'jakaria';
+
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan. ' ' . $namaBelakang;
+
+echo "Nama Depan: {$namaDepan} <br>";
+echo 'Nama Belakang: ' . $namaBelakang . '<br>';
+
+echo $namaLengkap;
+>>>>>>> 0d11190 (Jobsheet 4)
 ?>
